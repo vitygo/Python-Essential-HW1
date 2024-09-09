ec4a08cebb1a86785ce24c0719e7a02a1368634a
Viktor <vitygocanal@gmail.com>
2024-09-09 19:15:45 +0200

    ex2 review class added

--- ex2.py
+++ ex2.py
@@ -1,5 +1,3 @@
-from colorama import Fore, Style
-
 class Book:
     def __init__(self, title:str, author:str, year:int, genre:str):
         self.title = title
@@ -9,33 +7,49 @@ class Book:
         self.reviews = []
 
     def __str__(self) -> str:
-        if self.reviews:
-            reviews_string = '\n'.join(self.reviews)
-        else:
-            reviews_string = f'NO AVALIABLE REVIEWES FOR "{self.title}" BOOK!'
-
         return (f"Title: {self.title} \n"
                 f"Author: {self.author}\n" 
                 f"Year: {self.year} \n"
-                f"Genre: {self.genre} \n\n"
-                f"Reviews:\n{reviews_string}")
+                f"Genre: {self.genre} \n"
+                f"\nВідгуки:"
+                )
 
-    def add_review(self, review:str, user_name:str='Unknown User'):
-        self.reviews.append(f"{Fore.RED}Користувач{Style.RESET_ALL} {Fore.YELLOW}{user_name}{Style.RESET_ALL}: {review}")
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def show_reviews(self): #новий метод виводу review
+        if self.reviews:
+            for review in self.reviews:
+                print(review)
+        else:
+             print(f'NO AVALIABLE REVIEWES FOR "{self.title}" BOOK!')
+
+class Review: #добавив класс для Review
+    def __init__(self, user_review:str, user_name:str = "Unknown user"):
+        self.user_name = user_name
+        self.user_review = user_review
+    
+    def __str__(self) -> str:
+        return f"{self.user_name}: {self.user_review}"
+    
 
 
 book1 = Book("The Hobbit", "J.R.R. Tolkien", 1937, "Fantasy")
 book2 = Book("1984", "George Orwell", 1949, "Dystopian")
 
+review1 = Review(user_name='Мішаня', user_review='Чьотінька книжка, респект автору і уважуха')
+review2 = Review(user_name='Kitty23', user_review='Лайк люблю толкіена')
+review3 = Review(user_name='Дарт Вейдер',user_review='haha good good very good')
+review4 = Review(user_name='Анатолій',user_review='продам гараж')
+review5 = Review(user_review='Класно!')
 
-book1.add_review(user_name='Мішаня', review='Чьотінька книжка, респект автору і уважуха')
-book1.add_review(user_name='Kitty23', review='Лайк люблю толкіена')
-book1.add_review(user_name='Дарт Вейдер', review='haha good good very good')
-book1.add_review(user_name='Анатолій', review='продам гараж')
-book1.add_review(review='Класно!')
+book1.add_review(review1)
+book1.add_review(review2)
+book1.add_review(review3)
+book1.add_review(review4)
+book1.add_review(review5)
 
 
 
 print(book1)
-print()
-print(book2)
+book1.show_reviews()
