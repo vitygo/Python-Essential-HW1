e0cad1226776e8e781a8d2152f75fdf5a9f9b8f1
Viktor <vitygocanal@gmail.com>
2024-09-10 12:51:20 +0200

    ex4 fixed

--- ex4.py
+++ ex4.py
@@ -1,38 +1,54 @@
 from colorama import Fore, Style
 
-class CarSalon:
+class  CarDealership:
     def __init__(self, name:str):
         self.name = name
         self.cars = []
     
     def __str__(self) -> str:
-        ...
+        return f"Салон: {self.name}\n"
 
     def add_car(self, car:"Car"):
-        ...
-
-    def sell_car(self):
-        ...
+        self.cars.append(car)
         
 
+    def show_cars(self):
+        if self.cars:
+            print('Доступні авто: ')
+            for num, car in enumerate(self.cars):
+                print(f"({num+1}), {car}")
+        else:
+             print(f'Немає доступних автівок на {self.name}')
+
+    def sell_car(self):
+        print(f"{len(self.cars)} авто в доступі")
+        self.show_cars()
+        user_choice = int(input("Оберіть авто по номеру: "))
+        sold =self.cars.pop(user_choice-1)
+        print(f"{sold.make} {sold.model} продано")
+        
+        
 
 class Car:
-    def __init__(self, model:str, year:int, price:int):
+    def __init__(self, make:str, model:str, year:int, price:int):
+        self.make = make
         self.model = model
         self.year = year
         self.price = price
 
     def __str__(self) -> str:
-        return f"Машина: {self.model}\nРік: {self.year}\nЦіна: ${self.price}\n"
+        return (f"Марка: {self.make}\n"
+                f"Модель: {self.model}\n"
+                f"Рік: {self.year}\n"
+                f"Ціна: ${self.price}\n"
+                )
     
- 
-autoria = CarSalon("AUTO.RIA")
-car1 = Car('BMW M3', 2019, 100000)
-car2 = Car('Porshe Panamera', 2015, 90000)
-
+autoria = CarDealership("AUTO.RIA")
+car1 = Car('BMW', 'm3', 2019, 100000)
+car2 = Car('Porshe', 'Panamera', 2015, 90000)
 autoria.add_car(car1)
 autoria.add_car(car2)
-
+autoria.show_cars()
 autoria.sell_car()
 
 print(autoria)
